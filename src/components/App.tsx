6a91e61647a5f80aebb3f9c99f5902d4504a6b07
YourContender <dream-boy123@ukr.net>
2023-10-09 18:32:15 +0300

    init commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { FC, useState, useEffect, useRef } from "react";
+
+const App: FC = () => {
+    return (
+        <div>
+            Hello motherfucker!
+        </div>
+    )
+}
+
+export default App;
